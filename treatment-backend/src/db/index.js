44e9fb26761a78b61fdd13b2991fd1973711b5b0
deepak gupta <dg91918@gmail.com>
2025-06-29 21:20:55 +0530

    direct mongo url

--- treatment-backend/src/db/index.js
+++ treatment-backend/src/db/index.js
@@ -2,7 +2,9 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 
 dotenv.config();
-const mongo_url = process.env.MONGODB_URL;
+// const mongo_url = process.env.MONGODB_URL;
+const mongo_url =
+  "mongodb+srv://deepak:zATksVZoxgA1TRL0@deepakecom.vybffzz.mongodb.net/test?retryWrites=true&w=majority";
 console.log("MongoDB URL:", mongo_url);
 const connectDB = async () => {
   try {
